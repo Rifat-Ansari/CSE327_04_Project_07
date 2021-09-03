53c7e1723c2b0e7beaae5774b2a80cd7641ad778
Jannat Ara Ferdouse Raya <59131687+Jannat-Ara@users.noreply.github.com>
2021-09-04 00:50:39 +0600

    Welcome Page

---
+++ fanush01/lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            
+            children:<Widget>[
+              Column(
+                children:<Widget>[
+                  Text(
+                    "Fanush",
+                      style:TextStyle(
+                      color:Color(0xffF78E26),
+                      fontWeight: FontWeight.bold,
+                      fontSize:30,
+                    ),
+                  ),
+                  SizedBox(
+                    height:20,
+                  ),
+                  Text("Welcome to the Streaming Platform-Fanush",
+                  textAlign:TextAlign.center,
+                  style: TextStyle(
+                    color:Colors.orange[300],
+                    fontSize:15,
+                  ),)
+                ],
+              ),
+              Container(
+                height:MediaQuery.of(context).size.height/2,
+                decoration:BoxDecoration(
+                  image: DecorationImage(
+                    image:AssetImage("")
+                  )
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  // the login button
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      //Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+                    },
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        color: Colors.black
+                      ),
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    child:Text(
+                      "Login",
+                      style:TextStyle(
+                        color:Colors.orange[700],
+                        fontWeight: FontWeight.w600,
+                        fontSize:18
+                      ),
+                    ),
+                  ),
+                //SignUpButton Starts
+                  SizedBox(height: 20),
+                  MaterialButton(
+                    minWidth:double.infinity,
+                    height:60,
+                    onPressed:(){
+                      //Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage()));
+                    },
+                    color:Color(0xffF78E26),
+                    shape:RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)
+                      ),
+                      child:Text(
+                      "Sign Up",
+                      style:TextStyle(
+                        color:Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize:18
+                      ),
+                    ),
+                  )
+                ],
+              )
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
